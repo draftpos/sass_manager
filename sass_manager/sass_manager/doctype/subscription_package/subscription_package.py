8d843c081780f3bb65f783bed79c55650820609e
Nasirucode <akingbolahan12@gmail.com>
2026-01-18 08:48:57 +0100

    readygit add .

--- sass_manager/sass_manager/doctype/subscription_package/subscription_package.py
+++ sass_manager/sass_manager/doctype/subscription_package/subscription_package.py
@@ -9,20 +9,20 @@ class SubscriptionPackage(Document):
 	# begin: auto-generated types
 	# This code is auto-generated. Do not modify anything in this block.
 
-	from typing import TYPE_CHECKING
+	# from typing import TYPE_CHECKING
 
-	if TYPE_CHECKING:
-		from frappe.types import DF
-		from sass_manager.sass_manager.doctype.package_feature.package_feature import PackageFeature
+	# if TYPE_CHECKING:
+	# 	from frappe.types import DF
+	# 	from sass_manager.sass_manager.doctype.package_feature.package_feature import PackageFeature
 
-		currency: DF.Link
-		description: DF.TextEditor | None
-		features: DF.Table[PackageFeature]
-		is_active: DF.Check
-		max_users: DF.Int
-		package_code: DF.Data
-		package_name: DF.Data
-		price: DF.Currency
+	# 	currency: DF.Link
+	# 	description: DF.TextEditor | None
+	# 	features: DF.Table[PackageFeature]
+	# 	is_active: DF.Check
+	# 	max_users: DF.Int
+	# 	package_code: DF.Data
+	# 	package_name: DF.Data
+	# 	price: DF.Currency
 	# end: auto-generated types
 
 	pass
