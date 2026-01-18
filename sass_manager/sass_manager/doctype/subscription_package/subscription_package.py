9356b27bb0ca0496260afea675755e54dd60f633
frappe <frappe@vmi2845022.contaboserver.net>
2026-01-18 09:03:56 +0100

    update subscription package doctype

--- sass_manager/sass_manager/doctype/subscription_package/subscription_package.py
+++ sass_manager/sass_manager/doctype/subscription_package/subscription_package.py
@@ -1,28 +1,9 @@
-# Copyright (c) 2025, nasirucode and Contributors
-# License: MIT. See LICENSE
+# Copyright (c) 2026, nasirucode and contributors
+# For license information, please see license.txt
 
-import frappe
+# import frappe
 from frappe.model.document import Document
 
 
 class SubscriptionPackage(Document):
-	# begin: auto-generated types
-	# This code is auto-generated. Do not modify anything in this block.
-
-	# from typing import TYPE_CHECKING
-
-	# if TYPE_CHECKING:
-	# 	from frappe.types import DF
-	# 	from sass_manager.sass_manager.doctype.package_feature.package_feature import PackageFeature
-
-	# 	currency: DF.Link
-	# 	description: DF.TextEditor | None
-	# 	features: DF.Table[PackageFeature]
-	# 	is_active: DF.Check
-	# 	max_users: DF.Int
-	# 	package_code: DF.Data
-	# 	package_name: DF.Data
-	# 	price: DF.Currency
-	# end: auto-generated types
-
 	pass
---
+++ sass_manager/sass_manager/doctype/subscription_package/test_subscription_package.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, nasirucode and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSubscriptionPackage(FrappeTestCase):
+	pass
