558b0c0ccca5c36f6289cea5bb584501f7470576
Josphat Ndhlovu <josphatkndhlovu@gmail.com>
2026-03-31 18:05:06 +0200

    fea:updated user

--- sass_manager/sass_manager/api/models/site_model.py
+++ sass_manager/sass_manager/api/models/site_model.py
@@ -41,7 +41,7 @@ class SiteModel:
                 verify=False,
                 allow_redirects=True
             )
-            return response.status_code < 200
+            return response.status_code == 200
         except (ConnectionError, Timeout, RequestException):
             return False
         except Exception:
